467290458fa2a53e4266ee80c5c3ae62cf451820
eaCe <webmaster@slaps.de>
2022-01-07 21:20:37 +0100

    feat: hide width select if form is ignored

--- boot.php
+++ boot.php
@@ -18,6 +18,9 @@ if (rex::isBackend() && rex::getUser()) {
 if(rex_url::currentBackendPage() === 'index.php?page=yform/manager/table_field') {
     rex_extension::register('YFORM_GENERATE', function (rex_extension_point $ep) {
         $subject = $ep->getSubject();
-        $subject->setValueField('choice', ['yform_ui_width',rex_i18n::msg('yform_ui_width'),YUi::getSelectWidths(),'0','0','','','','','','','','','0']);
+
+        if(!YUi::isIgnored($subject->objparams['form_hiddenfields']['table_name'])) {
+            $subject->setValueField('choice', ['yform_ui_width',rex_i18n::msg('yform_ui_width'),YUi::getSelectWidths(),'0','0','','','','','','','','','0']);
+        }
     });
 }
