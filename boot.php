9fee03060415e74fceca3cc55c2d99ec007867cd
eaCe <webmaster@slaps.de>
2022-01-14 14:12:48 +0100

    fix: check if field type is value

--- boot.php
+++ boot.php
@@ -19,7 +19,7 @@ if(rex_url::currentBackendPage() === 'index.php?page=yform/manager/table_field')
     rex_extension::register('YFORM_GENERATE', function (rex_extension_point $ep) {
         $subject = $ep->getSubject();
 
-        if(!YUi::isIgnored($subject->objparams['form_hiddenfields']['table_name'])) {
+        if(!YUi::isIgnored($subject->objparams['form_hiddenfields']['table_name']) && rex_get('type_id') === 'value') {
             $subject->setValueField('choice', ['yform_ui_width',rex_i18n::msg('yform_ui_width'),YUi::getSelectWidths(),'0','0','','','','','','','','','0']);
         }
     });
